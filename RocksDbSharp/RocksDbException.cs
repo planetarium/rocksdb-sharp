8073766eff574ac9d9c4cd09e1d65c2779b1618f
Lee Dogeon <dev.moreal@gmail.com>
2021-02-18 11:48:57 +0900

    refactor: read utf8 string directly

--- RocksDbSharp/RocksDbException.cs
+++ RocksDbSharp/RocksDbException.cs
@@ -7,9 +7,27 @@ namespace RocksDbSharp
     public class RocksDbException : RocksDbSharpException
     {
         public RocksDbException(IntPtr errptr)
-            : base(Encoding.UTF8.GetString(Encoding.Unicode.GetBytes(Marshal.PtrToStringUni(errptr))))
+            : base(ReadNullTerminatedString(errptr, Encoding.UTF8))
         {
             Native.Instance.rocksdb_free(errptr);
+        }
+
+        private static unsafe string ReadNullTerminatedString(IntPtr ptr, Encoding encoding)
+        {
+            int size = 0;
+            for (byte* p = (byte*)ptr.ToPointer(); *p != 0; ++p)
+            {
+                ++size;
+            }
+
+            byte[] buffer = new byte[size];
+            Marshal.Copy(
+                ptr,
+                buffer,
+                0,
+                size);
+
+            return encoding.GetString(buffer);
         }
     }
 }
